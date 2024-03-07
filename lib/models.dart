15f4c75facbcfa224d097f863f168343f9fa885d
Julia Ilasova <1julka1il@gmail.com>
2024-03-07 10:43:05 +0100

    added icons depending on the weather forecast

--- lib/models.dart
+++ lib/models.dart
@@ -1,4 +1,3 @@
-
 class WeeklyForecastDto {
   double? latitude;
   double? longitude;
--- lib/weekly_forecast_list.dart
+++ lib/weekly_forecast_list.dart
@@ -10,7 +10,6 @@ class WeeklyForecastList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final DateTime currentDate = DateTime.now();
     final TextTheme textTheme = Theme.of(context).textTheme;
 
     return SliverList(
@@ -20,36 +19,7 @@ class WeeklyForecastList extends StatelessWidget {
           return Card(
             child: Row(
               children: <Widget>[
-                SizedBox(
-                  height: 200.0,
-                  width: 200.0,
-                  child: Stack(
-                    fit: StackFit.expand,
-                    children: <Widget>[
-                      DecoratedBox(
-                        position: DecorationPosition.foreground,
-                        decoration: BoxDecoration(
-                          gradient: RadialGradient(
-                            colors: <Color>[
-                              Colors.grey[800]!,
-                              Colors.transparent
-                            ],
-                          ),
-                        ),
-                        child: Image.network(
-                          'https://images.unsplash.com/photo-1709603945846-6901ed447ecd?q=80&w=2429&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Center(
-                        child: Text(
-                          weeklyForecast.time?.elementAt(index) ?? 'Date',
-                          style: textTheme.displayMedium,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                _buildImageOverlay(context, textTheme, index),
                 Expanded(
                   child: Padding(
                     padding: const EdgeInsets.all(20.0),
@@ -57,7 +27,11 @@ class WeeklyForecastList extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Text(
-                          'Monday',
+                          _weekdayAsString(
+                            DateTime.parse(
+                              weeklyForecast.time!.elementAt(index),
+                            ),
+                          ),
                           style: textTheme.headlineMedium,
                         ),
                         const SizedBox(height: 10.0),
@@ -72,7 +46,7 @@ class WeeklyForecastList extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Text(
-                    '${weeklyForecast.temperature2MMax!.elementAt(index)} | ${weeklyForecast.temperature2MMin!.elementAt(index)} F',
+                    '${weeklyForecast.temperature2MMax!.elementAt(index)} | ${weeklyForecast.temperature2MMin!.elementAt(index)} C',
                     style: textTheme.titleMedium,
                   ),
                 ),
@@ -83,5 +57,132 @@ class WeeklyForecastList extends StatelessWidget {
         childCount: 7,
       ),
     );
+  }
+
+  Widget _buildImageOverlay(
+      BuildContext context, TextTheme textTheme, int index) {
+    final imageSize = MediaQuery.of(context).size.width * 0.3;
+    return SizedBox(
+      height: imageSize,
+      width: imageSize,
+      child: _getImageByWeatherCode(
+          weeklyForecastDto.daily!.weatherCode!.elementAt(index)),
+      /* child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          DecoratedBox(
+            position: DecorationPosition.foreground,
+            decoration: BoxDecoration(
+              gradient: RadialGradient(
+                colors: <Color>[Colors.grey[800]!, Colors.transparent],
+              ),
+            ),
+            child: _getImageByWeatherCode(
+              weeklyForecastDto.daily!.weatherCode!.elementAt(index),
+            ),
+          ),
+          Center(
+            child: Text(
+              '',
+              style: textTheme.displayMedium,
+            ),
+          ),
+        ],
+      ),*/
+    );
+  }
+
+  String _weekdayAsString(DateTime time) {
+    return switch (time.weekday) {
+      DateTime.monday => 'Monday',
+      DateTime.tuesday => 'Tuesday',
+      DateTime.wednesday => 'Wednesday',
+      DateTime.thursday => 'Thursday',
+      DateTime.friday => 'Friday',
+      DateTime.saturday => 'Saturday',
+      DateTime.sunday => 'Sunday',
+      _ => ''
+    };
+  }
+
+  Image _getImageByWeatherCode(int weatherCode) {
+    String url = '';
+    switch (weatherCode) {
+      // Clear
+      case 0:
+        url =
+            'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-01-1024.png';
+        break;
+
+      // Mainly clear, partly cloudy
+      case 1:
+      case 2:
+        url =
+            'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-02-512.png';
+        break;
+      // Overcast
+      case 3:
+        url =
+            'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-22-512.png';
+        break;
+
+      // Fog
+      case 45:
+      case 48:
+        url =
+            'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-27-1024.png';
+        break;
+
+      // Drizzle
+      case 51:
+      case 53:
+      case 55:
+        url =
+            'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-30-512.png';
+        break;
+
+      // Freezing drizzle
+      case 56:
+      case 57:
+        url =
+            'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-30-512.png';
+        break;
+
+      // Rain slight and moderate intensity
+      case 61:
+      case 63:
+        url =
+            'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-31-512.png';
+        break;
+
+      // Rain heavy intensity and freezing rain
+      case 65:
+      case 66:
+        url =
+            'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-32-512.png';
+        break;
+
+      // Snow
+      case 71:
+      case 73:
+      case 75:
+      case 77:
+        url =
+            'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-24-1024.png';
+        break;
+
+      // Thunderstorm
+      case 95:
+      case 96:
+      case 99:
+        url =
+            'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-08-1024.png';
+        break;
+
+      default:
+        url =
+            'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-02-512.png';
+    }
+    return Image.network(url, fit: BoxFit.cover);
   }
 }
