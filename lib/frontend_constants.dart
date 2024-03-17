61122ab349a75050d6e4cd180963354a09220632
Julia Ilasova <1julka1il@gmail.com>
2024-03-17 13:08:45 +0100

    styling changes

--- lib/frontend_constants.dart
+++ lib/frontend_constants.dart
@@ -1,6 +1,12 @@
 class DailyForecastIcons {
   static const String wind =
       'https://cdn3.iconfinder.com/data/icons/wind-4/512/N_T_938Artboard_1_copy_2-512.png';
-  static const String rainChance = 'https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_44-512.png';
-  static const String placeholder = 'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-01-1024.png';
+  static const String rainChance =
+      'https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_44-512.png';
+  static const String sunrise =
+      'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-07-256.png';
+  static const String sunset =
+      'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-09-512.png';
+  static const String placeholder =
+      'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-01-1024.png';
 }
--- lib/screens/daily_forecast/daily_forecast_screen.dart
+++ lib/screens/daily_forecast/daily_forecast_screen.dart
@@ -44,21 +44,15 @@ class _DailyForecastScreenState extends State<DailyForecastScreen> {
                     const SizedBox(height: 60),
                     Text('City Name',
                         style: Theme.of(context).textTheme.headlineLarge),
-                    const SizedBox(
-                      height: 20,
-                    ),
                     Text(
                       widget.dateString,
                       style: Theme.of(context).textTheme.bodyMedium,
                     ),
+                    _spacer(),
                     _buildFirstRow(context),
-                    const SizedBox(
-                      height: 20,
-                    ),
+                    _spacer(),
                     _buildDailyOverview(context),
-                    const SizedBox(
-                      height: 20,
-                    ),
+                    _spacer(),
                     _buildHourlyForecast(context),
                   ],
                 ),
@@ -73,7 +67,9 @@ class _DailyForecastScreenState extends State<DailyForecastScreen> {
   Widget _buildDailyOverview(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(18), color: Colors.grey[200]),
+        borderRadius: BorderRadius.circular(18),
+        color: Colors.grey[200],
+      ),
       padding: const EdgeInsets.all(8),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -82,29 +78,35 @@ class _DailyForecastScreenState extends State<DailyForecastScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                _darkText('Sunrise', context),
-                _darkText('Sunset', context),
+                _textWithIcon(
+                    'Sunrise', DailyForecastIcons.sunrise, context, 50),
+                _textWithIcon('Sunset', DailyForecastIcons.sunset, context, 50),
               ],
             ),
-            const SizedBox(
-              height: 20,
-            ),
+            _spacer(),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _darkText('Wind km/h', context),
-                    _darkText('Gusts km/h', context),
-                  ],
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _darkText('Wind km/h', context),
+                      _divider(),
+                      _darkText('Gusts km/h', context),
+                    ],
+                  ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    _darkText('Precipation', context),
-                    _darkText('Humidity', context),
-                  ],
+                const Expanded(child: Row()),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      _darkText('Precipation', context),
+                      _divider(),
+                      _darkText('Humidity', context),
+                    ],
+                  ),
                 )
               ],
             )
@@ -115,30 +117,45 @@ class _DailyForecastScreenState extends State<DailyForecastScreen> {
   }
 
   Widget _buildFirstRow(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Row(
-          children: [
-            SizedBox(
-              height: 100,
-              width: 100,
-              child: Image.network(
-                  'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-01-1024.png'),
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(18),
+        color: Colors.grey[200],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            flex: 6,
+            child: Row(
+              children: [
+                SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Image.network(
+                      'https://cdn2.iconfinder.com/data/icons/weather-color-2/500/weather-01-1024.png'),
+                ),
+                Text(
+                  '26',
+                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                        color: Colors.black,
+                      ),
+                ),
+              ],
             ),
-            Text(
-              '26',
-              style: Theme.of(context).textTheme.headlineLarge,
+          ),
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                _darkText('Max', context),
+                _divider(),
+                _darkText('Min', context),
+              ],
             ),
-          ],
-        ),
-        Column(
-          children: <Widget>[
-            Text('Max', style: Theme.of(context).textTheme.bodyMedium),
-            Text('Min', style: Theme.of(context).textTheme.bodyMedium),
-          ],
-        ),
-      ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -190,6 +207,20 @@ class _DailyForecastScreenState extends State<DailyForecastScreen> {
         ),
         _darkText(text, context),
       ],
+    );
+  }
+
+  Widget _spacer() {
+    return const SizedBox(
+      height: 16,
+    );
+  }
+
+  Divider _divider() {
+    return const Divider(
+      color: Colors.black,
+      thickness: 2,
+      height: 10,
     );
   }
 }
